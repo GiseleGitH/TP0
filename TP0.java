d95db1d17d3052d59855084b9476937cdac597b9
GiseleGitH <123955946+GiseleGitH@users.noreply.github.com>
2023-11-22 16:39:32 +0100

    Add files via upload

---
+++ TP0.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp0;
+
+import Point.Point;
+import Point.PointA;
+
+/**
+ *
+ * @author SG
+ */
+public class TP0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Point p = new Point(4,3);
+PointA p1 = new PointA("Gisèle",p.x,p.y);
+System.out.println(p);
+System.out.println(p1);
+    }
+    
+}
+    
+
